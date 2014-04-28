827059ab0461fb871470820d5258bba33db6b205
Daniel Romero <infoslack@gmail.com>
2014-04-28 17:28:48 -0300

    exercise 4

---
+++ src/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose! */
+
+int main()
+{
+        int age = 10;
+        int heigth;
+
+        printf("I am %d years old.\n");
+        printf("I am %d inches tall.\n", heigth);
+
+        return 0;
+}
