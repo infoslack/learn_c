0e88e07aeb3385f959ae71ece252476e42a0affa
Daniel Romero <infoslack@gmail.com>
2014-04-28 16:31:30 -0300

    exercise 3

---
+++ src/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+        int age = 10;
+        int heigth = 72;
+
+        printf("I am %d years old.\n", age);
+        printf("I am %d inches tall.\n", heigth);
+
+        return 0;
+}
